15a61d3df636a284e96b299e4df5ae2197e0f3b7
Anya <61061763+AnyaOrlova@users.noreply.github.com>
2021-11-09 21:03:10 +0700

    Add files via upload

---
+++ pvt_Lab_2/montecarlo/runge.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/gnuplot
+#!/usr/bin/gnuplot -persist
+
+set terminal png size 1200,800 linewidth 4 font "Verdana,14"
+set termoption dash
+set output "runge.png"
+set grid x y
+set xlabel "Number of processes"
+set ylabel "Relative speedup (2 processes)"
+set key left top
+unset log
+set xrange [0 : 70]
+set yrange [0 : 70]
+plot "linear.txt" using 1:2 with linespoints linecolor 4 title "Linear speedup", "10^7.txt" using 1:2 with linespoints linecolor 5 title "Parallel speedup for n=10^7", "10^8.txt" using 1:2 with linespoints linecolor 6 title "Parallel speedup for n=10^8"
